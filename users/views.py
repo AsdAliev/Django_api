ed7a577aee69ec833f09d6b7eed2d4fd454bede9
Asad_Aliev <Asdaliev@mail.ru>
2023-08-14 15:04:52 +0300

    Добавлена логика страницы регистрации

--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import User
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -22,4 +23,12 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
